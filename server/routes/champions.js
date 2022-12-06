f40d8a03593c3bf182fc1819a19c13638be2d16e
Antonio Obradović <55183634+AntonioObra@users.noreply.github.com>
2022-12-07 00:43:50 +0100

    removed server from this repo

--- server/routes/champions.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getChampions, getSingleChampion } from "../controllers/champions.js";
-const router = express.Router();
-
-router.get("/", getChampions);
-router.get("/:id", getSingleChampion);
-
-export default router;
--- server/routes/comments.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createComment, deleteComment } from "../controllers/comments.js";
-
-const router = express.Router();
-
-router.post("/", createComment);
-router.delete("/:id", deleteComment);
-
-export default router;
